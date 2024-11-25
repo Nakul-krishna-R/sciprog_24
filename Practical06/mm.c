54362319e8594c1d4ff44984a027dd92bb01f061
Nakul-krishna-R <nakulkrishna96@gmail.com>
2024-11-25 19:30:29 +0000

    Update mm.c

--- Practical06/mm.c
+++ Practical06/mm.c
@@ -4,5 +4,5 @@ void matmult(int n, int p , int q, double A[n][p], double B[p][q], double C[n][q
     for(i=0;i<n;i++)
         for(j=0;j<q;j++)
             for(k=0;k<p;k++)
-                C[i][j] = C[i][j]+A[i][k]+B[k][j];
+                C[i][j] = C[i][j]+A[i][k]*B[k][j];
 }
